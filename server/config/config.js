c9b9d5017f3fdb081a93d2906815c75bb96191c6
NinoJose <jose.nino@qoopa.co>
2019-05-30 09:47:18 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,32 @@
+//import { url } from "inspector";
+
 /*
  * Puerto 
  */
 
 process.env.PORT = process.env.PORT || 3000;
+
+/*
+ * Entorno
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/*
+ * Base de datos 
+ */
+
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+
+    urlDB = 'mongodb+srv://innome:hHzdo2REoxH4v3dE@cluster0-ifysd.mongodb.net/cafe';
+
+}
+
+process.env.urlDB = urlDB;
